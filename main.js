2fdc54f086d83f2ebc298408807023349241a87a
sasince2023 <sasince286@gmail.com>
2024-08-17 21:38:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+// For this basic example, we won't include any JavaScript functionality,
+// but this file can be used to add interactive features later, such as a cart system or a dynamic product filter.
